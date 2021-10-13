e0cccc94665724a6f117d022903b0e0d149f2b05
Vlad Korneev <74673738+veenrokdalv@users.noreply.github.com>
2021-10-14 02:41:56 +0300

    Change field model

--- app/models/telegram_chat_member_rating.py
+++ app/models/telegram_chat_member_rating.py
@@ -6,7 +6,7 @@ class TelegramChatMemberRating(BaseModel):
     __tablename__ = 'telegram_chat_member_ratings'
 
     id = sa.Column(sa.BigInteger(), sa.Sequence('telegram_chat_member_ratings_id_seq'), primary_key=True)
-    from_chat_member = sa.Column(sa.ForeignKey('telegram_chat_members.id'))
-    in_chat_member = sa.Column(sa.ForeignKey('telegram_chat_members.id'))
+    from_chat_member_id = sa.Column(sa.ForeignKey('telegram_chat_members.id'))
+    in_chat_member_id = sa.Column(sa.ForeignKey('telegram_chat_members.id'))
     value = sa.Column(sa.Float(2, True, 2), nullable=False)
     status = sa.Column(sa.String(32))
